194176fdc593b1f5c4e3be9bc22c2be9bdcdcc60
linfayang <linfayangs@gmail.com>
2022-02-13 00:02:22 +0800

    收件人

--- js/letter.js
+++ js/letter.js
@@ -15,7 +15,7 @@ $("#open").click(function () {
 		new Typed('.letter', {
 			strings: [
 				"^1000Dear&nbsp;&nbsp;" + content.to,
-				content.recipient + "^200<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+				content.recipient + "^200(๑′ᴗ‵๑)<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
 				content.text + "<br><br><p style='float:right; display:block; width:" + 
 				content.sign + "px;'>^1000" +
 				content.from + "^200</p>"
